2b2122b102b828e3d86d218b11a19fc2e2646a74
ambikaChandavolu <ambikasoni@yahoo.com>
2022-05-16 16:15:37 -0500

    Tested toString Handles Empty fields

--- src/main/java/org/launchcode/techjobs/oo/CoreCompetency.java
+++ src/main/java/org/launchcode/techjobs/oo/CoreCompetency.java
@@ -2,17 +2,18 @@ package org.launchcode.techjobs.oo;
 
 import java.util.Objects;
 
-public class CoreCompetency {
+public class CoreCompetency extends JobField{
 
-    private int id;
+   /* private int id;
     private static int nextId = 1;
     private String value;
+    */
 
-    public CoreCompetency() {
-        this.id = nextId;
-        nextId++;
+    public CoreCompetency(String value) {
+        super(value);
     }
 
+   /*
     public CoreCompetency(String value) {
         this();
         this.value = value;
@@ -45,6 +46,6 @@ public class CoreCompetency {
 
     public String getValue(){ return value; }
     public void setValue(String value){ this.value = value; }
-
+*/
 
 }
