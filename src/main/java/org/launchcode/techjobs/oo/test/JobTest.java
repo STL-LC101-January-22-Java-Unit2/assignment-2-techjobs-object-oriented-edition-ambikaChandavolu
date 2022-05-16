472ef8ae1225d2948d55a1da5fd1eb4b85b46827
ambikaChandavolu <ambikasoni@yahoo.com>
2022-05-15 23:06:58 -0500

    Tested Equals Method

--- src/main/java/org/launchcode/techjobs/oo/test/JobTest.java
+++ src/main/java/org/launchcode/techjobs/oo/test/JobTest.java
@@ -39,8 +39,15 @@ public class JobTest {
 
         assertEquals("Persistence", job.getCoreCompetency().getValue());
         assertTrue(job.getCoreCompetency() instanceof CoreCompetency);
+    }
 
+    @Test
+    public void testJobsForEquality(){
+        Job job1 = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+        Job job2 = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+        assertFalse(job1.equals(job2));
 
     }
+
 
 }
