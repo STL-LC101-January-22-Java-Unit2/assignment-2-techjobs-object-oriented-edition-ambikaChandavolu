0de72299f51d87b11ce9d52b1da6280ee7b2c12a
ambikaChandavolu <ambikasoni@yahoo.com>
2022-05-15 22:30:35 -0500

    Tested Empty Constructor

--- src/main/java/org/launchcode/techjobs/oo/PositionType.java
+++ src/main/java/org/launchcode/techjobs/oo/PositionType.java
@@ -1,5 +1,7 @@
 package org.launchcode.techjobs.oo;
 
+import java.util.Objects;
+
 public class PositionType {
 
     private int id;
@@ -14,12 +16,27 @@ public class PositionType {
     public PositionType(String value) {
         this();
         this.value = value;
+        System.out.println("Value is : " + value);
     }
 
     // TODO: Add a custom toString() method that returns the data stored in 'value'.
 
+    @Override
+    public String toString(){ return value; }
+
     // TODO: Add custom equals and hashCode methods. Consider two PositionType objects "equal" when
     //  their id fields match.
+
+    @Override
+    public boolean equals(Object o){
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        PositionType pt = (PositionType) o;
+        return id == pt.id;
+    }
+
+    @Override
+    public int hashCode(){ return Objects.hash(id);  }
 
     // Getters and Setters:
 
--- src/main/java/org/launchcode/techjobs/oo/test/JobTest.java
+++ src/main/java/org/launchcode/techjobs/oo/test/JobTest.java
@@ -1,12 +1,24 @@
 package org.launchcode.techjobs.oo.test;
 
+import static org.junit.Assert.*;
+
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
+import org.junit.Assert;
+import org.junit.Test;
+import org.launchcode.techjobs.oo.*;
 
 /**
  * Created by LaunchCode
  */
 @RunWith(JUnit4.class)
 public class JobTest {
+
+    @Test
+    public void testSettingJobId(){
+        Job job1 = new Job();
+        Job job2 = new Job();
+        assertTrue(job1.getId() != job2.getId());
+    }
 
 }
